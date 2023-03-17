0a3b74e7b62946151af078d22a2f33d3ce129077
Matheus-commit <matheusnunes67@gmail.com>
2023-03-17 15:36:49 -0300

    Introduzindo o back-end nas activities. Implementado o ViewBinding no gradle.

--- app/src/main/java/com/rode_legal_pneus/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/rode_legal_pneus/ui/activities/LoginActivity.kt
@@ -9,7 +9,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
 
-        //TesteFIreBase
+
 
     }
 
--- app/src/main/java/com/rode_legal_pneus/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/rode_legal_pneus/ui/activities/RegisterActivity.kt
@@ -3,10 +3,22 @@ package com.rode_legal_pneus.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rode_legal_pneus.R
+import com.rode_legal_pneus.databinding.RegisterScreenBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var  binding: RegisterScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_screen)
+        binding = RegisterScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonregister.setOnClickListener {
+
+            val email1 = binding.email.text.toString()
+            val senha = binding.pass.text.toString()
+
+        }
     }
 }
