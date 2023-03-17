d88adc0002da916a194088c59bdc775f6959d506
Matheus-commit <matheusnunes67@gmail.com>
2023-03-17 14:58:10 -0300

    Criado as Activities de cada item do Layout

---
+++ app/src/main/java/com/rode_legal_pneus/ui/activities/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.rode_legal_pneus.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rode_legal_pneus.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_screen)
+    }
+}
