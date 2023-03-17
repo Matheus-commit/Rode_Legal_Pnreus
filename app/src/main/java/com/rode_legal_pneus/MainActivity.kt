2fdd68f3c90ac5f421bf3ccbff3e48f183841ea8
Matheus-commit <matheusnunes67@gmail.com>
2023-03-17 14:26:50 -0300

    First commit in Android Studio

--- app/src/main/java/com/rode_legal_pneus/MainActivity.kt
+++ app/src/main/java/com/rode_legal_pneus/MainActivity.kt
@@ -2,12 +2,15 @@ package com.rode_legal_pneus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.FirebaseCommonKtxRegistrar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
 
+        //TesteFIreBase
 
     }
 
