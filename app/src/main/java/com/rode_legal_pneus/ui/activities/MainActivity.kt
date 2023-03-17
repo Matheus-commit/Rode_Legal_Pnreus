3b50c4b59840d37af8b25289e250e31e63554f32
Matheus-commit <matheusnunes67@gmail.com>
2023-03-17 14:53:17 -0300

    Criado as Activities de cada item do Layout

---
+++ app/src/main/java/com/rode_legal_pneus/ui/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.rode_legal_pneus.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rode_legal_pneus.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_screen)
+    }
+
+
+}
