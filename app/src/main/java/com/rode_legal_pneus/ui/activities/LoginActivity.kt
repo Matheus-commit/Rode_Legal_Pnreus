3b8645fd15501f84da09b47129f4510e56da850c
Matheus <Matheus>
2023-03-19 16:07:22 -0300

    Back-end de login and register

--- app/src/main/java/com/rode_legal_pneus/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/rode_legal_pneus/ui/activities/LoginActivity.kt
@@ -1,33 +1,79 @@
 package com.rode_legal_pneus.ui.activities
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import com.rode_legal_pneus.R
 import com.rode_legal_pneus.databinding.LoginscreenBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: LoginscreenBinding
+    private val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LoginscreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.textNewAccount.setOnClickListener {
+        binding.btnLogin.setOnClickListener { view ->
+
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editPassword.text.toString()
+
+            if(email.isEmpty() || senha.isEmpty()){
+
+                var snackbar = Snackbar.make(view, "Preencha todos os campos!", Snackbar.LENGTH_SHORT)
+                snackbar.setBackgroundTint(Color.RED)
+                snackbar.show()
+
+            } else{
+
+                auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener { autenticacao ->
+
+                    if(autenticacao.isSuccessful)
+                        navegarTelaPrincipal()
+                }.addOnFailureListener {
+
+                    var snackbar = Snackbar.make(view, "Erro ao fazer login do usuário!", Snackbar.LENGTH_SHORT)
+                    snackbar.setBackgroundTint(Color.RED)
+                    snackbar.show()
+                }
+            }
+
+        }
+
+
+        binding.textNewAccount.setOnClickListener { view ->
 
             getRegisterScreen()
         }
 
     }
 
-    fun getRegisterScreen(){
+    fun navegarTelaPrincipal (){
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 
+    fun getRegisterScreen(){
         val intent = Intent(this, RegisterActivity::class.java)
         startActivity(intent)
+    }
 
+    override fun onStart (){
+        super.onStart()
+
+        val usuarioAtual = FirebaseAuth.getInstance().currentUser
+
+        if(usuarioAtual != null){
+            navegarTelaPrincipal()
+        }
     }
 
 }
--- app/src/main/java/com/rode_legal_pneus/ui/activities/RegisterActivity.kt
+++ app/src/main/java/com/rode_legal_pneus/ui/activities/RegisterActivity.kt
@@ -1,15 +1,23 @@
 package com.rode_legal_pneus.ui.activities
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.rode_legal_pneus.R
 import com.rode_legal_pneus.databinding.RegisterscreenBinding
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var  binding: RegisterscreenBinding
+    private val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,13 +28,47 @@ class RegisterActivity : AppCompatActivity() {
 
             var email = binding.editEmail.text.toString()
             var senha = binding.editPassword.text.toString()
+            var nome = binding.editName.text.toString()
 
-            if(email.isEmpty() || senha.isEmpty()){
+            if(email.isEmpty() || senha.isEmpty() || nome.isEmpty()){
 
                 var snackbar = Snackbar.make(view, "Preencha todos os campos", Snackbar.LENGTH_SHORT)
                 snackbar.setBackgroundTint(Color.RED)
                 snackbar.show()
+            }else{
+                auth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener { cadastro ->
+
+                    if (cadastro.isSuccessful){
+                        Toast.makeText(
+                            this@RegisterActivity,
+                            "Cadastro realizado com sucesso!",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        binding.editEmail.setText("")
+                        binding.editPassword.setText("")
+                        binding.editName.setText("")
+
+                        var intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+                }.addOnFailureListener { exception ->
+
+                    val mensagemErro = when(exception){
+                        is FirebaseAuthWeakPasswordException -> "Digite uma senha com no mínimo 6 caracteres"
+                        is FirebaseAuthInvalidCredentialsException -> "Digite um e-mail válido"
+                        is FirebaseAuthUserCollisionException -> "Esta conta já foi cadastrada"
+                        is FirebaseNetworkException -> "Sem conexão com a internet"
+                        else -> "Erro ao cadastrar usuário"
+                    }
+
+                    var snackbar = Snackbar.make(view, mensagemErro, Snackbar.LENGTH_SHORT)
+                    snackbar.setBackgroundTint(Color.RED)
+                    snackbar.show()
+                }
             }
         }
     }
+
+    //fun
 }
