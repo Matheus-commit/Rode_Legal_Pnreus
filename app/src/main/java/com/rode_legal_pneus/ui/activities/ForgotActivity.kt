71a04120fa6083333063f7d6efd5e48941a0ed4c
Matheus <Matheus>
2023-03-20 07:41:00 -0300

    Back-end forgot completo

--- app/src/main/java/com/rode_legal_pneus/ui/activities/ForgotActivity.kt
+++ app/src/main/java/com/rode_legal_pneus/ui/activities/ForgotActivity.kt
@@ -3,6 +3,7 @@ package com.rode_legal_pneus.ui.activities
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.rode_legal_pneus.R
@@ -19,33 +20,35 @@ class ForgotActivity : AppCompatActivity() {
         binding = ForgotScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var email = binding.editEmail.text.toString()
 
         binding.btnForgot.setOnClickListener { view ->
 
-                auth.sendPasswordResetEmail(email).addOnCompleteListener{ task ->
+            var email = binding.editEmail.text.toString()
 
-                    if(task.isSuccessful) {
+            if(email.isEmpty()){
 
-                        var snackbar = Snackbar.make(view, "E-mail enviado!", Snackbar.LENGTH_SHORT)
-                        snackbar.setBackgroundTint(Color.RED)
-                        snackbar.show()
+                var snackbar = Snackbar.make(view, "Preencha o campo de e-mail", Snackbar.LENGTH_SHORT)
+                snackbar.setBackgroundTint(Color.RED)
+                snackbar.show()
+
+            }else{
+                auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this@ForgotActivity, "Email enviado!", Toast.LENGTH_SHORT).show()
+                        finish()
 
                     }else{
-                        var snackbar = Snackbar.make(view, "Error!", Snackbar.LENGTH_SHORT)
-                        snackbar.setBackgroundTint(Color.RED)
-                        snackbar.show()
-
+                        Toast.makeText(this@ForgotActivity, task.exception!!.message, Toast.LENGTH_SHORT).show()
                     }
 
                 }
+
             }
-
-
         }
 
-    }
 
+    }
+}
 //if(email.isEmpty()){
 //
 //    var snackbar = Snackbar.make(view, "Preencha o campo de e-mail", Snackbar.LENGTH_SHORT)
